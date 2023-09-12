fc9b2166cf01f63dea498a4ff588269236166a41
oranc <krani1004@gmail.com>
2023-09-12 10:29:29 +0900

    junit Test java.lang.NoClassDefFoundError 오류 수정 추가

--- spro02/src/test/java/com/keduit/controller/BoardControllerTest.java
+++ spro02/src/test/java/com/keduit/controller/BoardControllerTest.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -16,8 +16,9 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml",
-	"fifile:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
 @Log4j
 public class BoardControllerTest {
 	@Setter(onMethod_ = {@Autowired})
