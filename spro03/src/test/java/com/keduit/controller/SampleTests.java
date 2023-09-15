7d518491aefa3d34406a105e0ea57bc45fc606bf
oranc <krani1004@gmail.com>
2023-09-15 13:55:39 +0900

    03폴더 백업

---
+++ spro03/src/test/java/com/keduit/controller/SampleTests.java
@@ -0,0 +1,58 @@
+package com.keduit.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.google.gson.Gson;
+import com.keduit.domain.Ticket;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@Log4j
+public class SampleTests {
+	@Setter(onMethod_ = @Autowired)
+	private WebApplicationContext ctx;
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testConvert() throws Exception {
+		// 티켓을 (json으로 변형 후)전달 해주면서 postman의 역할 하도록 구현
+		Ticket ticket = new Ticket();
+		ticket.setTno(123);
+		ticket.setOwner("Hong");
+		ticket.setGrade("AAA");
+		
+		String jsonStr = new Gson().toJson(ticket);
+		
+		log.info(jsonStr);
+		
+		// 축소 가능한 방법으로 perform	내에 작성한 경우
+		mockMvc.perform(post("/sample/ticket")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonStr))
+				.andExpect(status().is(200));
+	}
+}
