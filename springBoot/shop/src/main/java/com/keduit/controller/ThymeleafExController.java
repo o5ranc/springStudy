6e6bcd0c0f138bd7596aee7e27951b63a78b3e60
oranc <krani1004@gmail.com>
2023-09-22 12:13:48 +0900

    spring-boot-devtools 의존성 추가

---
+++ springBoot/shop/src/main/java/com/keduit/controller/ThymeleafExController.java
@@ -0,0 +1,16 @@
+package com.keduit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafExController {
+    @GetMapping("/ex01")
+    public String thymeleafEx01(Model model) {
+        model.addAttribute("data", "타임리프 예제입니다.");
+        return "thymeleafEx/thymeleafEx01";
+    }
+}
