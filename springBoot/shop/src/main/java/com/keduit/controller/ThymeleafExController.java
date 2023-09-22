e594f09247370ceced4d0543c1399d1af82585c1
oranc <krani1004@gmail.com>
2023-09-22 14:14:16 +0900

    thymeleaf 사용한 매개변수 받아서 웹페이지에 출력 확인

--- springBoot/shop/src/main/java/com/keduit/controller/ThymeleafExController.java
+++ springBoot/shop/src/main/java/com/keduit/controller/ThymeleafExController.java
@@ -1,16 +1,50 @@
 package com.keduit.controller;
 
+import com.keduit.dto.ItemDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/thymeleaf")
 public class ThymeleafExController {
-    @GetMapping("/ex01")
+    @GetMapping(value = "/ex01")
     public String thymeleafEx01(Model model) {
         model.addAttribute("data", "타임리프 예제입니다.");
         return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDTO itemDto = new ItemDTO();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model) {
+        List<ItemDTO> itemDtoList = new ArrayList<>();
+
+        for(int i  = 0;i < 10; i++) {
+            ItemDTO itemDto = new ItemDTO();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setPrice(50000 + i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
     }
 }
